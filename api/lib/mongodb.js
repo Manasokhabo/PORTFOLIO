e0a493116d3896241c11b32b406620e8031516a2
Manas sen <102599901+Manasokhabo@users.noreply.github.com>
2026-02-20 01:08:11 -0800

    Update mongodb.js

--- api/lib/mongodb.js
+++ api/lib/mongodb.js
@@ -1,25 +1,30 @@
-
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
 
 const uri = process.env.MONGODB_URI;
-const options = {};
 
-let client;
-let clientPromise;
-
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
+if (!uri) {
+  throw new Error('Please add your Mongo URI to Vercel Environment Variables');
 }
 
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
   }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri).then((mongoose) => {
+      console.log('MongoDB Connected via Mongoose');
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
-export default clientPromise;
+export default dbConnect;
